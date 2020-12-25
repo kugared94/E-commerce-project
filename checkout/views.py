781753ef7d800808ed3d16958e5a8ceccd8d71ec
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-12-25 15:01:57 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I2HOYE4kGEdcX14iOgwueltPuAEvy1eIHzegldk8tH8ONYLNdozCNrnkxcbZN5314vlpMXVkjtvPu3rZbqZffjm00FIV2junK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
