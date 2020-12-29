94aece7e86edc446517dab93c3b4af35523d0a36
muhammad syafiq bin abdul Razak <syafiq_razak@hotmail.com>
2020-12-29 08:38:21 +0000

    Removed secret key and set debug

--- mad_commerce/settings.py
+++ mad_commerce/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b8wbc(95q#b@2x6wu9%8$y6v9k@#8l^b43mspq^7zu6&9pi_0!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kugared94-e-commerce-project.herokuapp.com', 'localhost']
 
